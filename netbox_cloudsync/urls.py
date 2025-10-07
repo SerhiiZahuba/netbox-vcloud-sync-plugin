bc75233b13b5351ffa8f3bbdfbfad6713898b1ea
sergio_from_tauri <36307887+SerhiiZahuba@users.noreply.github.com>
2025-10-07 14:00:53 +0300

    Create urls.py

---
+++ netbox_cloudsync/urls.py
@@ -0,0 +1,13 @@
+from netbox.plugins import PluginTemplateExtension
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.CloudSyncConfigListView.as_view(), name='cloudsyncconfig_list'),
+    path('add/', views.CloudSyncConfigEditView.as_view(), name='cloudsyncconfig_add'),
+    path('<int:pk>/', views.CloudSyncConfigView.as_view(), name='cloudsyncconfig'),
+    path('<int:pk>/edit/', views.CloudSyncConfigEditView.as_view(), name='cloudsyncconfig_edit'),
+    path('<int:pk>/delete/', views.CloudSyncConfigDeleteView.as_view(), name='cloudsyncconfig_delete'),
+    path('<int:pk>/changelog/', views.CloudSyncConfigChangeLogView.as_view(), name='cloudsyncconfig_changelog'),
+    path('<int:pk>/run/', views.RunSyncNowView.as_view(), name='run_sync_now'),
+]
