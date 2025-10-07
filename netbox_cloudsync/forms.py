5fb4dfd7417cb8dbfaf49ee6a1aee28155adedac
sergio_from_tauri <36307887+SerhiiZahuba@users.noreply.github.com>
2025-10-07 14:02:55 +0300

    Add CloudSyncConfigForm for CloudSyncConfig model

---
+++ netbox_cloudsync/forms.py
@@ -0,0 +1,16 @@
+from netbox.forms import NetBoxModelForm
+from dcim.models import Site, DeviceRole, Platform
+from virtualization.models import Cluster
+from tenancy.models import Tenant
+from .models import CloudSyncConfig
+
+
+class CloudSyncConfigForm(NetBoxModelForm):
+    class Meta:
+        model = CloudSyncConfig
+        fields = (
+            'name', 'vcloud_url', 'vcloud_user', 'vcloud_password',
+            'sync_interval_minutes', 'sync_templates', 'sync_poweroff',
+            'netbox_site', 'netbox_cluster', 'netbox_role', 'netbox_tenant', 'netbox_platform',
+            'last_sync', 'next_sync', 'enabled'
+        )
